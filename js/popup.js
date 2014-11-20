e75ec333fde917478426c85e69e8505fd7473764
abaddonGIT <abaddongit@gmail.com>
2014-11-20 23:18:40 +0400

    first commit

---
+++ js/popup.js
@@ -0,0 +1,17 @@
+/**
+ * Created by abaddon on 20.11.2014.
+ */
+/*
+ global window, document
+ */
+(function (w, d) {
+    "use strict";
+    var FontConverter = function (strategy) {
+        this.strategy = strategy;
+    };
+
+    var FontConverterStrategy = function () {};
+
+
+
+}(window, document));
